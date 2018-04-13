62d868c033b9c264fc9cbb1ce530f53877a17db1
Notyour Business <not@your.business>
2018-04-13 20:09:19 +0200

    Rate created

--- Cryptaxation/Models/Rate.cs
+++ Cryptaxation/Models/Rate.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Cryptaxation.Models
 {
-    class Rate
-    {
-    }
+	public class Rate
+	{
+		public DateTime Date { get; set; }
+		public Currency OriginCurrency { get; set; }
+		public Currency DestinationCurrency { get; set; }
+		public Decimal Value { get; set; }
+	}
+
+	public enum Currency
+	{
+		SEK,
+		USD,
+		EUR,
+		BTC
+	}
 }
+
